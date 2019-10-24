06ea9f204cfcfd6c9342ec5c242d3cb3c583a777
michaelcohen716 <michaelcohen716@gmail.com>
2019-10-23 21:59:13 -0400

    update config

--- truffle-config.js
+++ truffle-config.js
@@ -22,10 +22,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-const mnemonic =
-  "visual jump vague melt ramp arrow police fire siege candy tag any";
-
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
